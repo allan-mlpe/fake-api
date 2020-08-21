efa87839e450092c10936d09102f33f88d1b49eb
allan-mlpe <allan_mlpe@hotmail.com>
2020-08-21 08:20:22 -0300

    Refactoring response build

--- src/index.js
+++ src/index.js
@@ -5,15 +5,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const buildResponse = (req, error = null) => {
+    const responseData = {
+        data: req.body,
+        headers: req.headers
+    };
+    
+    return error ? 
+        {
+            ...responseData,
+            error
+        } : responseData;
+}
+
 app.get('/', (req, res) => {
     res.send('Server running!');
 });
 
 app.post('/ok', (req, res) => {
-    const data = req.body;
-    const headers = req.headers;
+    const response = buildResponse(req);
 
-    return res.send({ data, headers });
+    return res.send(response);
 });
 
 app.post('/not-modified', (req, res) => {
@@ -21,23 +33,19 @@ app.post('/not-modified', (req, res) => {
 });
 
 app.post('/bad-request', (req, res) => {
-    const data = req.body;
-    const headers = req.headers;
-    const error = {
-        message: 'Bad request'
-    };
+    const response = buildResponse(
+        req, { message: 'Bad request' }
+    );
     
-    return res.status(400).send({ data, headers, error });
+    return res.status(400).send(response);
 });
 
 app.post('/internal-server-error', (req, res) => {
-    const data = req.body;
-    const headers = req.headers;
-    const error = {
-        message: 'Internal server error'
-    };
+    const response = buildResponse(
+        req, { message: 'Internal server error' }
+    );
     
-    return res.status(500).send({ data, headers, error });
+    return res.status(500).send(response);
 });
 
 app.listen(3000, '0.0.0.0');
